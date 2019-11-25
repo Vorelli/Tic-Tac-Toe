b7fd54bfa9f0410e334f5f97e727d560e97b722c
Natale Toscano <kickalliance195@gmail.com>
2019-11-25 15:42:30 -0500

    Game managager detects win condition
    Now the game manager can see if a player has three in a row
    (horizontally, vertically, or diagonally).
    
    Planned features:
        Win animation
        Player objects
        The ability for players to choose their 'piece'
        Point system
        A best of three/five option for friends to verse

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,15 @@
 const ticTacToeManager = (function() {
     const numPlayers = 2;
+    const possibleMatches = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
 
     function renderBoard() {
         for(i in gameBoard.getBoard())
@@ -11,13 +21,48 @@ const ticTacToeManager = (function() {
         renderBoard();
     }
 
-    const newGame = () => {
+    function newGame() {
         for(let i = 0; i < sideLength*sideLength; i++)
             setChar(i, " ");
         playerChoice();
     };
 
-    return {renderBoard};
+    function _win(matches) {
+        matches.forEach(element => {
+            document.querySelector(`#a${element}`).classList.add('win');
+        })
+        console.log('win');
+    }
+
+    function _match(positions) {
+        const chars = [
+            document.querySelector(`#a${positions[0]}`).firstElementChild.textContent,
+            document.querySelector(`#a${positions[1]}`).firstElementChild.textContent,
+            document.querySelector(`#a${positions[2]}`).firstElementChild.textContent
+        ];
+        return !(chars[0] == '' || chars[1] == '' || chars[2] == '' || chars[0] != chars[1] || chars[0] != chars[2]);
+    }
+
+    function checkForMatches() {
+        while(possibleMatches.forEach(element => {
+                if(_match(element))
+                    _win(element);
+        }));
+    }
+
+    function boxClicked(event) {
+        setChar(event.target.id[1], 'X');
+        checkForMatches();
+    }
+
+    function start() {
+        document.querySelectorAll('#gameBoard div').forEach(element => {
+            element.addEventListener('click', boxClicked)
+        });
+    }
+
+    return {start};
+
 })()
 
 const gameBoard = (function() {
@@ -32,4 +77,4 @@ const gameBoard = (function() {
 
 
 
-ticTacToeManager.renderBoard();
+ticTacToeManager.start();

aa95c854a65461211698c3726b54be8048a89064
Natale Toscano <kickalliance195@gmail.com>
2019-11-28 02:37:07 -0500

    Code refactor complete; Functions cleaner/clearer
    Separated code based on MVC model. Took a bit of work, but I was able
    to simplify a lot of code. I was going on and on about how to send
    winner object so that the 'win' class name would be added at the end of
    a game. Ended up just sending whoever was playing when the game was won.
    
    Planned features:
        Ability for the system to detect a stalemate and display a message
        Win animations
        A best of three/five option for friends to verse
        Maybe a 1-player (bot) modified
        Fix up the look of everything

--- scripts/script.js
+++ scripts/script.js
@@ -1,147 +1,85 @@
 const ticTacToeManager = (function() {
-    const numPlayers = 2;
     const players = [];
     let playing = false;
     let playersTurn;
+    const sideLength = 3;    
+    const board = [];
     const possibleMatches = [
         [0,1,2],[3,4,5],[6,7,8],
         [0,3,6],[1,4,7],[2,5,8],
         [0,4,8],[2,4,6]
     ];
+    const _switchPlayerTurn = () => playersTurn = players[0] == playersTurn ? players[1] : players[0];
+    const _moveisValid = (index) => (board[index] == '' || board[index] == ' ');
 
-    function setChar(index, char) {
-        gameBoard.setChar(index, char);
-    }
-    
-    function getFromUser(message, conditions) {
-        let temp;
-        do temp = prompt(message);
-        while (eval(conditions));
-        return temp;
-    }
-
-    const noOneHas = (temp) => {
-        for(i in players) {
-            if(players[i].getSymbol()==temp) return false;
-        }
-        return true;
-    };
-
-    function playerChoice() {
-        players.splice(0,2);
-        for(let i = 0; i < numPlayers; i++){
-            let playerName = getFromUser(`Enter player ${i+1}'s name:`, `!temp || temp==' ' || temp.length==0`)
-            let j = 0;
-            let playerChar;
-            do {
-                if(j>0) alert('Your symbol cannot match another player\'s');
-                playerChar = getFromUser(`Enter a character to use as ${playerName}'s symbol:`, `!temp || temp==' ' || temp.length == 0 || temp.length > 1`);
-                j=1;
-            } while(!noOneHas(playerChar))
-            players.push(player(playerName, playerChar))
-        }
-        playersTurn = players[0];
-    }
-
-    function newGame() {
-        clearBoard();
-        playerChoice();
+    function _clearBoard() {
+        for(let i = 0; i < sideLength*sideLength; i++)
+            board[i] = ' ';
+        _switchPlayerTurn();
+        playing=true;
+        displayManager.renderBoard();
         displayManager.renderScores();
-    };
+    }
 
     function _win(matches) {
-        matches.forEach(element => {
-            document.querySelector(`#a${element}`).classList.add('win');
-        })
+        playersTurn.win();
+        displayManager.setWinner(matches, playersTurn)
         playing = false;
-        players.forEach(player => {
-            if(player.getSymbol() == document.querySelector(`#a${matches[0]}`).firstElementChild.textContent)
-                player.win();
-                displayManager.renderScores();
-                displayManager.setWinner(player)
-        })
     }
 
     function _match(positions) {
-        const chars = [
-            document.querySelector(`#a${positions[0]}`).firstElementChild.textContent,
-            document.querySelector(`#a${positions[1]}`).firstElementChild.textContent,
-            document.querySelector(`#a${positions[2]}`).firstElementChild.textContent
-        ];
-        return !(chars[0] == '' || chars[0] == ' ' || chars[1] == '' || chars[1] == ' ' || chars[2] == '' || chars[2] == ' ' || chars[0] != chars[1] || chars[0] != chars[2]);
+        const chars = [board[positions[0]],board[positions[1]],board[positions[2]]];
+        return !(chars[0] == ' ' || chars[1] == ' ' || chars[2] == ' ' || chars[0] != chars[1] || chars[0] != chars[2]);
     }
 
-    function checkForMatches() {
-        for(i in possibleMatches){
-            if(_match(possibleMatches[i])){
-                _win(possibleMatches[i]);
-                return true;
-            }
-        }
+    function _checkForMatches() {
+        for(i in possibleMatches) if(_match(possibleMatches[i])) return true;
         return false;
     }
 
-    function switchPlayerTurn() {
-        playersTurn = players[0] == playersTurn ? players[1] : players[0];
-    }
-
-    function start() {
-        displayManager.updateButtons();
-        displayManager.fixDisplay();
-        newGame();
-    }
-
-    function clearBoard() {
-        const sideLength = 3;
-        for(let i = 0; i < sideLength*sideLength; i++)
-            setChar(i, " ");
-        document.querySelectorAll('#gameBoard div').forEach(element => {
-            element.classList.remove('win');
-        })
-        playing=true;
-        switchPlayerTurn();
-        displayManager.renderScores();
-    }
-
-    function ping(message, args, event) {
-        console.log(message);
-        switch(message) {
-            case 'newGame': clearBoard();break;
-            case 'restart': newGame();break;
+    function _makeMove(index) {
+        if(_moveisValid(index) && playing){
+            board[index] = playersTurn.getSymbol();
+            if (_checkForMatches()) {
+                playing = false;
+                _win(possibleMatches[i]);
+            } else _switchPlayerTurn();
         }
     }
-    const getPlayers = () => players;
-    const getPlayersTurn = () => playersTurn;
-    const getPlaying = () => playing;
 
-    return {start, ping, getPlayers, getPlayersTurn, getPlaying};
+    function _start(p) {
+        players.splice(0,2);
+        players.push(...p);
+        _clearBoard();
+        displayManager.fixDisplay();
+    }
 
+    function ping(message, event, arg) {
+        console.log(message);
+        switch(message) {
+            case 'newGame': _clearBoard();break;
+            case 'boxClicked': _makeMove(event.id[1]);break;
+            case 'start': _start(ticTacToeController.playerChoice());break;
+        }
+    }
+
+    return {_clearBoard, ping, getBoard: (() => Array.from(board)), getPlayers : (() => players), getPlayersTurn : (() => playersTurn), getPlaying: (() => playing), numPlayers: () => 2};
 })()
 
-const gameBoard = (function() {
-    const board = [];
-    const getChar = (index) => board[index];
-    const setChar = (index, char) => board[index] = char;
-    const getBoard = () => board;
-
-    return {getChar, setChar, getBoard};
-})();
-
 function player(name, symbol, startingWins=0) {
-    const getSymbol = () => symbol;
-    const getName = () => name;
-    const getWins = () => startingWins;
-    const win = () => startingWins++;
-    const resetWins = () => startingWins = 0;
-    return {getSymbol, getName, getWins, win, resetWins}
+    return {getSymbol: () => symbol, getName: () => name, getWins: () => startingWins, win: () => startingWins++, resetWins: () => startingWins = 0}
 }
 
 const displayManager = function() {
-    const needsToBeCentered = [
-        '#buttons',
-        '#playerDisplay',
-        '#gameBoard'
-    ];
+    const needsToBeCentered = ['#buttons', '#playerDisplay', '#gameBoard'];
+    let winner;
+    
+    const clearBoard = function() {
+        for(let i = 0; i < document.querySelectorAll('#gameBoard div').length; i++) {
+            let element = document.querySelectorAll('#gameBoard div')[i];
+            element.classList.remove('win');
+        }
+    }
 
     const fixDisplay = function() {
         const marginLength = (window.innerWidth - 600)/2 + 'px';
@@ -151,10 +89,7 @@ const displayManager = function() {
         })
     }
     
-    const renderBoard = function() {
-        for(i in gameBoard.getBoard())
-            document.querySelector(`#a${i}`).firstElementChild.textContent = gameBoard.getChar(i);
-    }
+    const renderBoard = () => {for(let i=0;i<ticTacToeManager.getBoard().length;i++) document.querySelector(`#a${i}`).firstElementChild.textContent = ticTacToeManager.getBoard()[i]};
 
     const renderScores = () => {
         document.querySelector('#playerDisplay').innerHTML = '';
@@ -162,48 +97,75 @@ const displayManager = function() {
             let temp = document.createElement('div');
             temp.classList.add('notSelectable');
             temp.textContent = `(${ticTacToeManager.getPlayers()[i].getSymbol()}) ${ticTacToeManager.getPlayers()[i].getName()}: ${ticTacToeManager.getPlayers()[i].getWins()}`;
-            console.log(ticTacToeManager.playing);
+            temp.player = ticTacToeManager.getPlayers()[i];
             if(ticTacToeManager.getPlayers()[i] == ticTacToeManager.getPlayersTurn() && ticTacToeManager.getPlaying())
                 temp.classList.add('turn');
+            console.log(temp.player);
+            console.log(temp.player == winner);
+            if(!ticTacToeManager.getPlaying() && winner === temp.player)
+                temp.classList.add('win');
             document.querySelector('#playerDisplay').appendChild(temp);
         }
     }
 
-    const setWinner = (player) => {
-        document.querySelectorAll('#playerDisplay div').forEach(element => {
-            if(element.textContent[1] == player.getSymbol())
-                console.log(element.classList)
-                element.classList.remove('turn');
-                element.classList.add('win');
-        })
+    const setWinner = (matches, player) => {
+        winner = !player ? undefined : player;
+        for(i in matches) document.querySelector(`#a${matches[i]}`).classList.add('win');
     }
-    return {fixDisplay, renderBoard, renderScores, setWinner};
+    return {fixDisplay, renderBoard, renderScores, setWinner, clearBoard};
 }();
 
 const ticTacToeController = function() {
+    const players = [];
     const updateButtons = () => {
         document.querySelectorAll('#buttons div').forEach(button => {
-            button.addEventListener('click', button.textContent == 'New Game' ? ticTacToeManager.ping.bind(button, 'newGame') : ticTacToeManager.ping.bind(button, 'restart'));
+            button.addEventListener('click', button.textContent == 'New Game' ? 
+            () => {
+                ticTacToeManager.ping('newGame');
+                displayManager.clearBoard();
+            }
+            : ticTacToeManager.ping.bind('start'));
         })
     }
+    
+    function _getFromUser(message, conditions) {
+        let temp;
+        do temp = prompt(message);
+        while (eval(conditions));
+        return temp;
+    }
 
-    function boxClicked(event) {
-        if(playing) {
-            if(gameBoard.getBoard()[event.target.id[1]] == '' || gameBoard.getBoard()[event.target.id[1]] == ' ') {
-                setChar(event.target.id[1], playersTurn.getSymbol());
-            }else return;
-            if(!checkForMatches()) switchPlayerTurn();
-            displayManager.renderScores();
+    const _noOneHas = (char) => {
+        if(char==' ') return false;
+        for(i in players) if(players[i].getSymbol()==char) return false;
+        return true;
+    };
+
+    function playerChoice() {
+        players.splice(0,2);
+        for(let i = 0; i < ticTacToeManager.numPlayers(); i++){
+            let playerName = _getFromUser(`Enter player ${i+1}'s name:`, `!temp || temp==' ' || temp.length==0`)
+            let j = 0;
+            let playerChar = ' ';
+            for(let j = 0; !_noOneHas(playerChar); j++) {
+                if(j>0) alert('Your symbol cannot match another player\'s');
+                playerChar = _getFromUser(`Enter a character to use as ${playerName}'s symbol:`, `!temp || temp==' ' || temp.length == 0 || temp.length > 1`);
+            }
+            players.push(player(playerName, playerChar))
         }
-        if(!(gameBoard.getBoard().indexOf('') == -1 || gameBoard.getBoard().indexOf(' ') == -1)) playing = false;
-        displayManager.renderBoard();
+        return players;
     }
 
     document.querySelectorAll('#gameBoard div').forEach(element => {
-        element.addEventListener('click', boxClicked)
+        element.addEventListener('click', () => {
+            ticTacToeManager.ping('boxClicked', element,);
+            displayManager.renderBoard();
+            displayManager.renderScores();
+        })
     });
     updateButtons();
+    return {playerChoice};
 }()
 
 window.onresize = displayManager.fixDisplay;
-ticTacToeManager.start();
+ticTacToeManager.ping('start');
